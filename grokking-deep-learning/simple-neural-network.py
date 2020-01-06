b39b48554daf6f6d00911b3d4410352357f90886
Sunil Udupi <udpsunil@gmail.com>
2020-01-06 22:11:50 +0530

    multiple inputs simple nn

--- grokking-deep-learning/simple-neural-network.py
+++ grokking-deep-learning/simple-neural-network.py
@@ -1,14 +1,25 @@
-# Simple neural network 
+# Simple neural network with multiple inputs
+
+def w_sum(a, b):
+    assert(len(a) == len(b))
+    output = 0
+    for x,y in zip(a,b):
+        output += x * y
+    return output
 
 # step 1: An empty network
-weight = 0.1
+weights = [0.1, 0.2, 0]
 
-def neural_network(input, weight):
-    prediction = input * weight
+def neural_network(input, weights):
+    prediction = w_sum(input, weights)
     return prediction
 
 # step 2: inserting one input datapoint
-number_of_toes = [8.5, 9.5, 10, 9]
-input = number_of_toes[0]
-prediction = neural_network(input, weight)
+toes = [8.5, 9.5, 9.9, 9.0]
+wlrec = [0.65, 0.8, 0.8, 0.9]
+nfans = [1.2, 1.3, 0.5, 1.0]
+
+input = [toes[0], wlrec[0], nfans[0]]
+prediction = neural_network(input, weights)
 print(prediction)
+
