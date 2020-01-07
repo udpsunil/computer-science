b54243c4af60815dd83fdf567b55ec008deb59f0
Sunil Udupi <udpsunil@gmail.com>
2020-01-07 20:52:09 +0530

    neural network with numpy

--- grokking-deep-learning/simple-neural-network.py
+++ grokking-deep-learning/simple-neural-network.py
@@ -1,24 +1,17 @@
-# Simple neural network with multiple inputs
-import vector_math
+# Simple neural network with multiple inputs numpy code
 
-def w_sum(a, b):
-    assert(len(a) == len(b))
-    output = vector_math.vector_sum(vector_math.elementwise_multiplication(a,b))
-    return output
-
-# step 1: An empty network
-weights = [0.1, 0.2, 0]
+import numpy as np
+weights = np.array([0.1, 0.2, 0])
 
 def neural_network(input, weights):
-    prediction = w_sum(input, weights)
-    return prediction
+    pred = input.dot(weights)
+    return pred
 
-# step 2: inserting one input datapoint
-toes = [8.5, 9.5, 9.9, 9.0]
-wlrec = [0.65, 0.8, 0.8, 0.9]
-nfans = [1.2, 1.3, 0.5, 1.0]
 
-input = [toes[0], wlrec[0], nfans[0]]
-prediction = neural_network(input, weights)
-print(prediction)
+toes = np.array([8.5, 9.5, 9.9, 9.0])
+wlrec = np.array([0.65, 0.8, 0.8, 0.9])
+nfans = np.array([1.2, 1.3, 0.5, 1.0])
 
+input = np.array([toes[0], wlrec[0], nfans[0]])
+pred = neural_network(input, weights)
+print(pred)
